a62d81f3f1e253235ded5baba49e0c22b594377d
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-05-25 23:58:46 +0100

    create progress stages config

---
+++ scripts/onboarding.js
@@ -0,0 +1,44 @@
+let currentProgress = 0
+const btnSteps = document.querySelectorAll('.next-button')
+
+function showProgressTab(n) {
+  let progress = document.getElementsByClassName('progress-tab')
+  progress[n].style.display = 'flex'
+
+  fixStepIndicators(n)
+}
+
+function nextPrev(n) {
+  let progress = document.getElementsByClassName('progress-tab')
+  progress[currentProgress].style.display = 'none'
+
+  currentProgress = currentProgress + n
+
+  if (currentProgress >= progress.length) {
+    console.log('finish, change for another page here')
+    return false
+  }
+
+  showProgressTab(currentProgress)
+}
+
+function fixStepIndicators(n) {
+  let i,
+    x = document.getElementsByClassName('step')
+
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(' active', '')
+  }
+
+  x[n].className += ' active'
+}
+
+window.addEventListener('load', () => {
+  showProgressTab(currentProgress)
+
+  btnSteps.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      nextPrev(1)
+    })
+  })
+})
