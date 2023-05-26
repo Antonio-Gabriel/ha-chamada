65674155796080b1cd41d064f0149dcdf44ada12
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-05-26 02:02:12 +0100

    indent the code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('sw.js');
-    });
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('sw.js')
+  })
 }
